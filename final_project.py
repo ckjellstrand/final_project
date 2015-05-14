7d3f605fa71aee33d27547fc9280484bb132e9ad
bjorn <bjorn@localhost.localdomain>
2015-05-14 18:29:43 -0400

    Added pocket_phi creation and integration for the probability the particle is in the pocket. Added some other qol stuff as well.

--- final_project.py
+++ final_project.py
@@ -66,16 +66,27 @@ def create_pocket_potential(epsilon, w, s, b, h):
     v[bottom:top] = b
 
     sfrac = s/2
-    slen = np.floor(num*sfrac)
-    v[bottom:bottom+slen] = h
-    v[top-slen:top] = h
+    slen = np.ceil(num*sfrac)
+    v[bottom-slen:bottom] = h
+    v[top:top+slen] = h
 
     return v
 
 
-def create_pocket_phi(epsilon):
+def create_pocket_phi(epsilon, w):
     num = (2*epsilon**-1) + 1
     xx = np.linspace(-1, 1, num)
+    phi = np.zeros(len(xx))
+
+    wfrac = w/2
+    wlen = np.floor(num*wfrac)
+    center = np.floor(len(xx)/2)
+    bottom = center-wlen
+    top = center+wlen+1
+
+    phi[bottom:top] = np.sqrt(2/w)*np.sin((np.pi/w)*xx[bottom:top])
+    print phi
+    return phi
 
 
 def plot_phi(func):
@@ -97,3 +108,44 @@ def plot_phi(func):
     subplots[2].set_ylabel("Im(Phi)")
     subplots[2].set_xlabel("X")
     f.show()
+
+
+def plot_v(func):
+    xx = np.linspace(-1, 1, len(func))
+    plt.plot(xx,np.real(func))
+    plt.title("Potential versus distance")
+    plt.ylabel("V")
+    plt.xlabel("x")
+    plt.plot(xx, func)
+    plt.show()
+
+
+def integrate_probability(epsilon, w, phi):
+    num = (2*epsilon**-1) + 1
+    xx = np.linspace(-1, 1, num)
+    phi = np.multiply(np.conjugate(phi), phi)
+
+    wfrac = w/2
+    wlen = np.floor(num*wfrac)
+    center = np.floor(len(xx)/2)
+    bottom = center-wlen
+    top = center+wlen+1
+
+    inside_pocket = phi[bottom:top]
+    outside_pocket_upper = phi[top+1:]
+    outside_pocket_lower = phi[:bottom]
+    inside_prob = np.sum(inside_pocket)
+    outside_prob = np.sum(outside_pocket_upper) + np.sum(outside_pocket_lower)
+
+    return inside_prob, outside_prob
+
+
+def find_prob_versus_time(phi, v, epsilon, w, steps):
+    a = []
+    b = []
+    for i in range(steps):
+        inside, outside = integrate_probability(epsilon, w, phi)
+        a.append(inside)
+        b.append(outside)
+        phi = find_nth_state(phi, 0.001, 1, v)
+    return a, b
